0dc4b557f3a429ef6cc0ef0128243573b928c47f
Christian-Garcia-H <chr40070gherr@gmail.com>
2026-07-18 23:50:52 -0500

    funcion para calcular el compromiso

--- programa.py
+++ programa.py
@@ -1,4 +1,3 @@
-
 datos = [
     ["AAA001", 210, 12],
     ["AAA002", 45, 5],
@@ -6,3 +5,14 @@ datos = [
     ["AAA004", 200, 2],
     ["AAA005", 150, 7]
 ]
+
+#funcion para calcular el compromiso de los clientes:
+def calcular_compromiso (duracion, clics):
+    if duracion > 180 and clics > 8:
+        return "Alto"
+    elif duracion < 60 or clics < 3:
+        return "Bajo"
+    else:
+        return "Medio"
+    
+    
